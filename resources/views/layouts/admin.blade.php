bc510eccffa732263ce7912965d314adc20605fd
AntaripSaha <antarip15@gmail.com>
2022-03-31 18:01:34 +0600

    admin layouts added

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -52,10 +52,8 @@
         </div>
       </li>
     </ul>
-
   </nav>
   <!-- /.navbar -->
-
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
@@ -63,7 +61,6 @@
       <img src="{{asset('backend/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Dashboard</span>
     </a>
-
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
@@ -76,15 +73,13 @@
         </div>
       </div> --}}
 
-
-
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item menu-open">
-            <a href="{{route('user.dashboard')}}" class="nav-link active">
+            <a href="{{route('admin.dashboard')}}" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
@@ -98,7 +93,6 @@
     </div>
     <!-- /.sidebar -->
   </aside>
-
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Main content -->
@@ -107,18 +101,14 @@
         @include('layouts.flash_messages')
         @yield('content')
       </div>
-
-
       @yield('adminlte')
     </div>
   </div>
-
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
   </aside>
   <!-- /.control-sidebar -->
-
   <!-- Main Footer -->
   <footer class="main-footer">
     <strong>Copyright &copy; 2022  </strong>
@@ -129,16 +119,12 @@
   </footer>
 </div>
 <!-- ./wrapper -->
-
 <!-- REQUIRED SCRIPTS -->
-
-
 <script>
     $('#myModal').on('shown.bs.modal', function () {
       $('#myInput').trigger('focus')
     })
-    </script>
-    
+</script>
 <script>
     function myFunction() {
         if(!confirm("Are You Sure to Delete This"))
